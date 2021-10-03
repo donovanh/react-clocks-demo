4efa3c98609fd719545d043e3f519e0b2dfad49f
Donovan Hutchinson <donovanh@gmail.com>
2021-10-03 14:36:16 +0100

    Updates

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,6 @@ import Clocks from "./components/Clocks";
 import AddClock from "./components/AddClock";
 import "./App.css";
 
-/*
- * Create branches for basic starting app, add in functionality:
- * Local state
- * Passing props
- * Shared state
- * Context API
- * API Calls with React Query
- * Reducers
- * Redux Toolkit
- * Testing React components using react-testing-library
- * Use context and provider with initial state
- * Dispatch to state from new clock component
- * -- List only the unused clocks in that component
- * Class-based components
- * API call to something like a quote of the day? Current temperature at each clock location?
- */
-
 export default function App() {
   return (
     <ClockContextProvider>
